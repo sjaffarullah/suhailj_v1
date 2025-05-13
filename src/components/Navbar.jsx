abccdd6ed4f7f6b79a9c7fdf68d6294e66a90af9
125998211-myseneca <sjaffarullah@myseneca.ca>
2025-05-12 21:20:43 -0400

    Updated navbar - added certifications

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
           "experience",
           "projects",
           "techstack",
-          "contact",
+          "certifications",
         ];
         let current = "home";
 
