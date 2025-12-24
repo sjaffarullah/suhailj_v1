d8c595376a07d271a94027b51e8f218cc59083dc
Suhail Jaffarullah <suhail.jaffarullah@gmail.com>
2025-12-23 20:57:02 -0500

    Updated experience

--- src/data/experience.js
+++ src/data/experience.js
@@ -6,4 +6,11 @@ export const EXPERIENCES = [
       description: "Provided technical support to end-users, resolving hardware, software, and network issues while managing user accounts and access permissions. Maintained IT documentation, oversaw inventory, and ensured timely resolution of incidents within SLAs, contributing to seamless IT operations.",
       technologies: ["Active Directory", "Microsoft Office 365", "ServiceNow ", "SCCM", "Azure" ],
     },
+    {
+      year: "Jan 2025 - Present",
+      role: "IT Technical Analyst",
+      company: "Wonderbrands",
+      description: "Provided technical support to end-users, resolving hardware, software, and network issues while managing user accounts and access permissions. Maintained IT documentation, oversaw inventory, and ensured timely resolution of incidents within SLAs, contributing to seamless IT operations.",
+      technologies: ["Active Directory", "Microsoft Office 365", "ServiceNow ", "SCCM", "Azure" ],
+    },
   ];
