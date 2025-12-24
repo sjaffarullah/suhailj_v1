579b02f3b3fe706ec2c58dd6595f7090e54ae051
Suhail Jaffarullah <suhail.jaffarullah@gmail.com>
2025-12-23 21:07:30 -0500

    Updated certs

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -1,10 +1,24 @@
-import cloupload from "../assets/certs/az900.png";
+import az900 from "../assets/certs/az900.png";
+import CC from "../assets/certs/CC.png"
+import CCP from "../assets/certs/CCP.png"
 
 export const CERTS = [
     {
       title: "Microsoft Certified: Azure Fundamentals",
       provider: "Microsoft",
-      image: cloupload,  
+      image: az900,  
       credUrl: "https://learn.microsoft.com/api/credentials/share/en-us/SuhailJaffarullah-7119/72E445EB7FE43FD0?sharingId=6032F335A46E218E",
+    },
+    {
+      title: "Certified in Cybersecurity (CC)",
+      provider: "ISC2",
+      image: CC,  
+      credUrl: "https://www.credly.com/badges/cee45a06-c5c4-484a-be6d-c0111dfcd1e6/linked_in_profile",
+    },
+    {
+      title: "AwS Certified Cloud Practitioner",
+      provider: "Amazon",
+      image: CCP,  
+      credUrl: "https://www.credly.com/badges/f5ca35e1-db53-402d-972d-3f5b261a2432/linked_in_profile",
     }
   ]; 
