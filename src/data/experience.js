9eefb20e0b5a9d2ab33ee17036537e802d75f0f2
Suhail Jaffarullah <suhail.jaffarullah@gmail.com>
2025-12-23 21:02:39 -0500

    Updated experience

--- src/data/experience.js
+++ src/data/experience.js
@@ -4,13 +4,13 @@ export const EXPERIENCES = [
       role: "IT Helpdesk",
       company: "FGF Brands Inc",
       description: "Provided technical support to end-users, resolving hardware, software, and network issues while managing user accounts and access permissions. Maintained IT documentation, oversaw inventory, and ensured timely resolution of incidents within SLAs, contributing to seamless IT operations.",
-      technologies: ["Active Directory", "Microsoft Office 365", "ServiceNow ", "SCCM", "Azure" ],
+      technologies: ["Active Directory", "Microsoft Office 365", "ServiceNow ", "Azure" ],
     },
     {
       year: "Jan 2025 - Present",
       role: "IT Technical Analyst",
       company: "Wonderbrands",
       description: "Provided technical support to end-users, resolving hardware, software, and network issues while managing user accounts and access permissions. Maintained IT documentation, oversaw inventory, and ensured timely resolution of incidents within SLAs, contributing to seamless IT operations.",
-      technologies: ["Active Directory", "Microsoft Office 365", "ServiceNow ", "SCCM", "Azure" ],
+      technologies: ["EntraID", "Intune", "M365", "Defender", "ServiceNow ", "SCCM", "Azure" ],
     },
   ];
