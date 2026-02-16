9f590bd50e6ef775b3386e1cec17462055ab9779
Suhail Jaffarullah <suhail.jaffarullah@gmail.com>
2026-02-16 18:39:57 -0500

    Updated experience

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,16 +1,16 @@
 export const EXPERIENCES = [
-    {
-      year: "May 2023 - Jan 2024",
-      role: "IT Helpdesk",
-      company: "FGF Brands Inc",
-      description: "Provided technical support to end-users, resolving hardware, software, and network issues while managing user accounts and access permissions. Maintained IT documentation, oversaw inventory, and ensured timely resolution of incidents within SLAs, contributing to seamless IT operations.",
-      technologies: ["Active Directory", "Microsoft Office 365", "ServiceNow ", "Azure" ],
-    },
-    {
-      year: "Jan 2025 - Present",
-      role: "IT Technical Analyst",
-      company: "Wonderbrands",
-      description: "Provided technical support to end-users, resolving hardware, software, and network issues while managing user accounts and access permissions. Maintained IT documentation, oversaw inventory, and ensured timely resolution of incidents within SLAs, contributing to seamless IT operations.",
-      technologies: ["EntraID", "Intune", "M365", "Defender", "ServiceNow ", "SCCM", "Azure" ],
-    },
-  ];
+  {
+    year: "Jan 2025 - Present",
+    role: "IT Technical Analyst",
+    company: "Wonderbrands",
+    description: "Provide technical support and systems analysis to optimize IT operations, resolve complex issues, and enhance infrastructure performance. Collaborate with cross-functional teams to implement solutions, strengthen security, and ensure reliable technology services.",
+    technologies: ["Microsoft Azure", "Microsoft Entra ID", "Microsoft Intune", "Microsoft 365", "ServiceNow", "SCCM", , "Microsoft Defender for Endpoint"],
+  },
+  {
+    year: "May 2023 - Jan 2024",
+    role: "IT Helpdesk",
+    company: "FGF Brands Inc",
+    description: "Delivered end-user support by resolving hardware, software, and network issues while managing accounts and access permissions. Maintained documentation, tracked inventory, and met SLA targets to support smooth IT operations.",
+    technologies: ["Active Directory", "Microsoft 365", "ServiceNow", "Microsoft Azure"],
+  },
+];
